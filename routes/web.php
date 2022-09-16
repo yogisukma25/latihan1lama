ac28862d697ba7af3a8c77bda8681b9fdcf660fe
Yogi sukma <yosukma257@gmail.com>
2022-09-16 07:45:18 +0700

    commit1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Yogi Sukmawati",
+        "email" => "Yogisukmawati@gmail.com",
+        "image" => "Yogi.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
